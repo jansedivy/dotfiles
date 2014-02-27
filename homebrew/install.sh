dcd83ad0acecb9c850cb431b759b490b0709fb54
Jan Sedivy <jansedivy@abdoc.net>
2014-02-27 12:52:14 +0100

    add hub into homebrew install script

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -6,7 +6,7 @@ then
   ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" > /tmp/homebrew-install.log
 fi
 
-brew install tmux grc coreutils reattach-to-user-namespace tmux-mem-cpu-load
+brew install hub tmux grc coreutils reattach-to-user-namespace tmux-mem-cpu-load
 brew install vim --override-system-vim
 
 exit 0
