d03bf4791120f38af61ec41cb8e25a2f06d3cc74
Jan Sedivy <jansedivy@abdoc.net>
2014-04-07 18:52:19 +0200

    add bunch of zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,19 +1,29 @@
-alias vi='vim'
-alias ios='open -a /Applications/Xcode.app/Contents/Applications/iPhone\ Simulator.app'
-
 alias activate='d=`pwd`; $(deactivate &>/dev/null); while [ "$d" != "" ]; do [ -f "$d"/venv/bin/activate ] && echo "activating $d/venv/" && source "$d/venv/bin/activate"; d=${d%/*}; done'
 
-alias dt='cd ~/.dotfiles'
-alias dr='cd ~/Dropbox'
+alias aliases='vim ~/.dotfiles/zsh/aliases.zsh'
+alias amend='git commit --amend'
+alias b='brew'
+alias bi='brew install'
+alias bu='brew update && brew upgrade'
 alias d='cd ~/Desktop'
-alias exp='cd ~/Dropbox/experiments'
 alias dev='cd ~/Dropbox/dev'
-
+alias dr='cd ~/Dropbox'
+alias drop='cd ~/Dropbox'
+alias dt='cd ~/.dotfiles'
+alias exp='cd ~/Dropbox/experiments'
+alias gap='git add --all -p'
+alias gcl='git clone'
+alias gco='git checkout'
+alias gl='gulp'
 alias gp='git push'
-alias gl='git pull'
-alias gap='git aa -p'
-
+alias ios='open -a /Applications/Xcode.app/Contents/Applications/iPhone\ Simulator.app'
+alias n='npm'
+alias ni='npm install'
 alias notes='cd ~/Dropbox/notes'
+alias review='git diff master'
+alias so='source ~/.dotfiles/zsh/aliases.zsh'
+alias u='cd ..'
+alias vi='vim'
 
 function g {
    if [[ $# > 0 ]]; then
