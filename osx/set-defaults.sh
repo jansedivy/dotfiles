6c32bcd364e5f2fefdef5b47e4a5f01a174f8188
Jan Sedivy <jansedivy@abdoc.net>
2014-04-21 21:53:09 +0200

    show ~/Library by default

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -1,3 +1,4 @@
 defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 defaults write NSGlobalDomain KeyRepeat -int 0
+chflags nohidden ~/Library
