f00baebbc796cc8b960d057d8e5f7a51e6dbffad
Jan Sedivy <jansedivy@abdoc.net>
2014-04-21 21:58:02 +0200

    remove bunch of stuff from path.zsh

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,8 +1,2 @@
-export PATH="./bin::/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/local/share/npm/bin:$PATH"
-
-export PATH="$PATH:/usr/local/mysql/bin"
-export PATH="$PATH:/usr/local/sbin"
-export PATH="/usr/local/bin:$PATH"
-export PATH="$HOME/.cabal/bin:$PATH"
-
+export PATH="/usr/local/bin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
