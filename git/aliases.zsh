4d2119cf1adeda5dc935d59109223465a209f83b
Jan Sedivy <jansedivy@abdoc.net>
2014-04-21 21:37:51 +0200

    move git alises into git aliases file

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -5,3 +5,10 @@ if [[ -f $hub_path ]]
 then
   alias git=$hub_path
 fi
+
+alias review='git diff master'
+alias gp='git push'
+alias amend='git commit --amend'
+alias gap='git add --all -p'
+alias gcl='git clone'
+alias gco='git checkout'
