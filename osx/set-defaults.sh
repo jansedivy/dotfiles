21604998541c1b5c5d7f5d7b4bc05491850ea778
Jan Sedivy <jansedivy@abdoc.net>
2014-05-18 13:31:44 +0200

    disable smart quotes

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -2,3 +2,9 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 defaults write NSGlobalDomain KeyRepeat -int 0
 chflags nohidden ~/Library
+
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
