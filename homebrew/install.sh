de2f529024313caef5f231ee82b499b692b0f10c
Jan Sedivy <jansedivy@abdoc.net>
2014-06-19 18:38:57 +0200

    install applications with brew cask

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -6,7 +6,29 @@ then
   ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" > /tmp/homebrew-install.log
 fi
 
+brew tap caskroom/cask
+brew tap caskroom/versions
+
+brew install brew-cask
 brew install zsh node hub tmux grc coreutils reattach-to-user-namespace ag
 brew install vim --override-system-vim
+
+brew cask install google-chrome-canary
+
+brew cask install firefox
+brew cask install firefox-nightly
+
+brew cask install spotify
+brew cask install iterm2
+brew cask install things
+brew cask install mou
+brew cask install teamviewer
+brew cask install coconutbattery
+brew cask install dropbox
+brew cask install gfxcardstatus
+brew cask install steam
+brew cask install skype
+brew cask install smcfancontrol
+brew cask install toggldesktop
 
 exit 0
