b4665dfd4502bcfa0157607f25f2ed8795355e83
Jan Sedivy <jansedivy@abdoc.net>
2015-02-21 13:52:19 +0100

    update homebrew install path

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -2,7 +2,7 @@
 if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew update
