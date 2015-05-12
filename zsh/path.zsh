d6a6539725f5d23785470289d0d8edd4711259c1
Jan Sedivy <jansedivy@abdoc.net>
2015-05-12 20:46:06 +0200

    add pyenv in path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,2 @@
-export PATH="/usr/local/bin:$ZSH/bin:$HOME/bin:/.rbenv/bin:$PATH"
+export PATH="~/.pyenv/shims:/usr/local/bin:$ZSH/bin:$HOME/bin:/.rbenv/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
