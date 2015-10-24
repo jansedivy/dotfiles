8a93f407c2c6c2d49f9b08cdb8347b94c56d4e1c
Jan Sedivy <jan.sedivy8@gmail.com>
2015-10-24 16:01:29 +0200

    update homebrew install script

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -16,15 +16,13 @@ brew install coreutils
 brew install reattach-to-user-namespace
 brew install ack
 brew install the_silver_searcher
-brew install selecta
-
 brew install jq
 
 npm install vmd -g
 npm install eslint -g
 npm install eslint-plugin-react -g
 
-brew install vim --override-system-vim
+brew install vim --override-system-vim --with-python3
 
 brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
@@ -36,7 +34,6 @@ brew cask install firefox-nightly 2> /dev/null
 
 brew cask install spotify 2> /dev/null
 brew cask install iterm2 2> /dev/null
-brew cask install things 2> /dev/null
 brew cask install transmission 2> /dev/null
 brew cask install imageoptim 2> /dev/null
 brew cask install openemu 2> /dev/null
@@ -50,18 +47,16 @@ brew cask install gfxcardstatus 2> /dev/null
 brew cask install steam 2> /dev/null
 brew cask install skype 2> /dev/null
 brew cask install smcfancontrol 2> /dev/null
-brew cask install toggldesktop 2> /dev/null
 brew cask install macdown 2> /dev/null
-brew cask install colorpicker-skalacolor 2> /dev/null
 brew cask install teamspeak-client 2> /dev/null
 brew cask install calibre 2> /dev/null
-brew cask install mailbox 2> /dev/null
-brew cask install launchrocket 2> /dev/null
 brew cask install cloudup 2> /dev/null
+brew cask install numi 2> /dev/null
 # cask install parallels 2> /dev/null
 
-
 brew tap caskroom/fonts
+
+brew cask install font-hack
 
 brew cleanup
 
