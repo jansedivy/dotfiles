5686d61accf4535b61cf0fb41889b74d60e50ec8
Jan Sedivy <jan.sedivy8@gmail.com>
2015-11-19 11:20:59 +0100

    add sbin to path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,3 @@
-export PATH="/usr/local/bin:$ZSH/bin:$HOME/bin:/.rbenv/bin:$PATH"
+export PATH="/usr/local/sbin:/usr/local/bin:$ZSH/bin:$HOME/bin:/.rbenv/bin:$PATH"
 export PATH="$PYENV_ROOT/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
