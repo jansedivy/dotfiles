314a98ac1defaf7e8873a7e171356694b355afb5
Jan Sedivy <me@jansedivy.com>
2018-09-08 18:00:29 +0200

    update

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,2 +1,8 @@
+#!/bin/sh
+if which brew >/dev/null 2>&1; then
+	brew install getantibody/tap/antibody || brew upgrade antibody
+else
+	curl -sL https://git.io/antibody | sh -s
+fi
 antibody bundle <"$ZSH/antibody/bundles.txt" >~/.zsh_plugins.sh
 antibody update
